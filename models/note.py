e009070983883d7fa64f36acb48c68432d0c2076
luizpetry <juniorpetry.k1@gmail.com>
2026-01-26 17:54:17 +0000

    feat: add User model and relate notes to users

--- models/note.py
+++ models/note.py
@@ -1,14 +1,28 @@
+from datetime import datetime
+from extensions import db
+
 class Note:
-    def __init__(self, id, title, content, created_at):
-        self.id = id
-        self.title = title
-        self.content = content
-        self.created_at = created_at
+    __tablename__ = "notes"
+
+    id = db.Column(db.Integer, primary_key = True)
+
+    title =  db.Column(db.String(120), nullable = False)
+
+    content = db.Column(db.Text, nullable = True, default = "")
+
+    created_at = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+
+    # FK - Foreign Key (Chave Estrangeira) para users.id - Se refere a primary key de outra tabela
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable = False)
+
+    # Relacionamento Reverso
+    user = db.relationship("User", back_populates = "notes")
     
     def to_dict(self):
         return {
             "id": self.id,
             "title": self.title,
             "content": self.content,
-            "created_at": self.created_at
+            "created_at": self.created_at.isoformat(),
+            "user_id": self.user_id,
         }
---
+++ models/user.py
@@ -0,0 +1,24 @@
+from extensions import db
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key = True)
+
+    name = db.Column(db.String(120), nullable = False)
+
+    # index = True - Melhora performance nas buscas por email
+    email = db.Column(db.String(255), unique = True, nullable = False, index = True)
+
+    # hash da senha (nunca retorna em respostas)
+    password_hash = db.Column(db.String(255), nullable = False)
+
+    # relacionamento: 1 user -> várias notas
+    notes = db.relationship("Note", back_populates="user", cascade="all, delete-orphan")
+
+    def to_dict(self):
+        return {
+            "id" : self.id,
+            "name" : self.name,
+            "email" : self.email,
+        }
