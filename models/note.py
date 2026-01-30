a006ef06ba99bcffca7084ed7b5166a570aae18d
luizpetry <juniorpetry.k1@gmail.com>
2026-01-30 17:41:17 +0000

    feat: add deleted_at, favorite and pinned fields to Note model

--- models/note.py
+++ models/note.py
@@ -1,16 +1,16 @@
 from datetime import datetime
 from extensions import db
 
-class Note:
+class Note(db.Model):
     __tablename__ = "notes"
 
     id = db.Column(db.Integer, primary_key = True)
-
     title =  db.Column(db.String(120), nullable = False)
-
     content = db.Column(db.Text, nullable = True, default = "")
-
     created_at = db.Column(db.DateTime, nullable = False, default = datetime.utcnow)
+    deleted_at = db.Column(db.DateTime, nullable=True)
+    is_favorite = db.Column(db.Boolean, nullable=False, default=False)
+    is_pinned = db.Column(db.Boolean, nullable=False, default=False)
 
     # FK - Foreign Key (Chave Estrangeira) para users.id - Se refere a primary key de outra tabela
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable = False)
@@ -24,5 +24,8 @@ class Note:
             "title": self.title,
             "content": self.content,
             "created_at": self.created_at.isoformat(),
+            "deleted_at": self.deleted_at.isoformat() if self.deleted_at else None,
+            "is_favorite": self.is_favorite,
+            "is_pinned": self.is_pinned,
             "user_id": self.user_id,
         }
--- models/user.py
+++ models/user.py
@@ -4,15 +4,12 @@ class User(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key = True)
-
     name = db.Column(db.String(120), nullable = False)
-
     # index = True - Melhora performance nas buscas por email
     email = db.Column(db.String(255), unique = True, nullable = False, index = True)
-
     # hash da senha (nunca retorna em respostas)
     password_hash = db.Column(db.String(255), nullable = False)
-
+    
     # relacionamento: 1 user -> várias notas
     notes = db.relationship("Note", back_populates="user", cascade="all, delete-orphan")
 
